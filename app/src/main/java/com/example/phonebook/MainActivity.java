d366e415c9707093cb65116010c22e916a88b3e0
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-06-14 18:41:55 +0200

    Create splashscreen

--- app/src/main/java/com/example/phonebook/MainActivity.java
+++ app/src/main/java/com/example/phonebook/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.phonebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // efter 3 sekunder går den vidare till Login
+    public void splashScreenTimer() {
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        }, 3000);
     }
 }
